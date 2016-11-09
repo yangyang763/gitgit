400988b8047de1ed1e141d2e11cd95a2858ed748
samsung <yangyang763@126.com>
2016-11-09 16:31:36 +0800

    第一次将代码进行提交，当前实现功能为：新建工程时的默认代码

---
+++ app/src/main/java/ay/qf/com/hady13_git2/MainActivity.java
@@ -0,0 +1,13 @@
+package ay.qf.com.hady13_git2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
